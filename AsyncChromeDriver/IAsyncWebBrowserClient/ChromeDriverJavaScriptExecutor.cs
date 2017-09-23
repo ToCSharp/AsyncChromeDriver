2eaa58d7de60e321cea6b116b5b20db43bd030d6
Oleg Zudov <tocsharp@gmail.com>
2017-09-23 18:55:50 +0300

    JavaScriptExecutor fix

--- AsyncChromeDriver/IAsyncWebBrowserClient/ChromeDriverJavaScriptExecutor.cs
+++ AsyncChromeDriver/IAsyncWebBrowserClient/ChromeDriverJavaScriptExecutor.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Zu.WebBrowser.AsyncInteractions;
 using System;
 using System.Collections.ObjectModel;
+using System.Collections;
 
 namespace Zu.Chrome
 {
@@ -44,17 +45,72 @@ namespace Zu.Chrome
         public async Task<object> ExecuteScript(string script, CancellationToken cancellationToken = default(CancellationToken), params object[] args)
         {
             var res = await asyncChromeDriver.WindowCommands.ExecuteScript(script, ArgsToStringList(args));
-            return ((res as JObject)?["value"] as JValue)?.Value;
+            if ((res as JObject)?["status"]?.ToString() == "13") throw new InvalidOperationException(ParseExecuteScriptReturnValue((res as JObject)?["value"])?.ToString());
+            return ParseExecuteScriptReturnValue((res as JObject)?["value"]); // (res as JObject)?["value"] as JValue)?.Value;
         }
 
         internal List<string> ArgsToStringList(object[] args)
         {
-            return args.Select(v =>
+            return args.Select(v => ArgToString(v)).ToList();
+        }
+
+        internal string ArgToString(object arg)
+        {
+            if (arg == null) return "null";
+            if (arg is bool) return (bool)arg ? "true" : "false";
+            if (arg is string) return $"'{(string)arg}'";
+            IDictionary dictionaryArg = arg as IDictionary;
+            if (dictionaryArg != null)
             {
-                if (v is bool) return (bool)v ? "true" : "false";
-                if (v is string) return $"'{(string)v}'";
-                return v.ToString();
-            }).ToList();
+                List<string> stringList = new List<string>();
+                foreach (DictionaryEntry kv in dictionaryArg)
+                {
+                    stringList.Add($"'{kv.Key}': {ArgToString(kv.Value)}");
+                }
+                return $"{{ {string.Join(", ", stringList)} }}";
+            }
+            if (arg is IDictionary<string, object>) return $"{{ {string.Join(", ", ((IDictionary<string, object>)arg).Select(v => ArgToString(v)))} }}";
+            if (arg is KeyValuePair<string, object>)
+            {
+                var kv = (KeyValuePair<string, object>)arg;
+                return $"{{ '{kv.Key}': {ArgToString(kv.Value)} }}";
+            }
+            IEnumerable enumerableArg = arg as IEnumerable;
+            if (enumerableArg != null)
+            {
+                List<object> objectList = new List<object>();
+                foreach (object item in enumerableArg)
+                {
+                    objectList.Add(item);
+                }
+                return $"[ {string.Join(", ", ArgsToStringList((objectList.ToArray())))} ]";
+            }
+            return arg.ToString();
+
+        }
+
+        private object ParseExecuteScriptReturnValue(JToken responseValue)
+        {
+            if (responseValue is JValue) return ((JValue)responseValue).Value;
+            if(responseValue is JArray)
+            {
+                var res = new List<object>();
+                foreach (var item in (JArray)responseValue)
+                {
+                    res.Add(ParseExecuteScriptReturnValue(item));
+                }
+                return res.ToArray();
+            }
+            else if(responseValue is JObject)
+            {
+                var res = new Dictionary<string, object>();
+                foreach (var item in (JObject)responseValue)
+                {
+                    res.Add(item.Key, ParseExecuteScriptReturnValue(item.Value));
+                }
+                return res;
+            }
+            return responseValue;
         }
     }
 }
