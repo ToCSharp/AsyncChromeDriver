57b07107137ec3dc780aa992ae68f814a5fa8e88
Oleg Zudov <tocsharp@gmail.com>
2018-03-07 20:41:19 +0300

    Chrome 65.0.3325.146 DevTools

---
+++ ChromeDevToolsClient/Log/EntryAddedEvent.cs
@@ -0,0 +1,20 @@
+namespace Zu.ChromeDevTools.Log
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Issued when new message was logged.
+    /// </summary>
+    public sealed class EntryAddedEvent : IEvent
+    {
+        /// <summary>
+        /// The entry.
+        /// </summary>
+        [JsonProperty("entry")]
+        public LogEntry Entry
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ ChromeDevToolsClient/Log/StartViolationsReportCommand.cs
@@ -0,0 +1,32 @@
+namespace Zu.ChromeDevTools.Log
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// start violation reporting.
+    /// </summary>
+    public sealed class StartViolationsReportCommand : ICommand
+    {
+        private const string ChromeRemoteInterface_CommandName = "Log.startViolationsReport";
+        
+        [JsonIgnore]
+        public string CommandName
+        {
+            get { return ChromeRemoteInterface_CommandName; }
+        }
+
+        /// <summary>
+        /// Configuration for violations.
+        /// </summary>
+        [JsonProperty("config")]
+        public ViolationSetting[] Config
+        {
+            get;
+            set;
+        }
+    }
+
+    public sealed class StartViolationsReportCommandResponse : ICommandResponse<StartViolationsReportCommand>
+    {
+    }
+}
